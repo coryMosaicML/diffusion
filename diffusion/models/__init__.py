c4e4a65200dc93a8e10e248980cbfc449ee408ab
Cory Stephenson <cory@mosaicml.com>
2023-06-13 21:38:18 -0700

    nvp experiment

--- diffusion/models/__init__.py
+++ diffusion/models/__init__.py
@@ -6,10 +6,11 @@
 from diffusion.models.models import continuous_pixel_diffusion, discrete_pixel_diffusion, stable_diffusion_2
 from diffusion.models.pixel_diffusion import PixelDiffusion
 from diffusion.models.stable_diffusion import StableDiffusion
-
+from diffusion.models.nvp import LNVP
 __all__ = [
     'continuous_pixel_diffusion',
     'discrete_pixel_diffusion',
+    'LNVP',
     'PixelDiffusion',
     'stable_diffusion_2',
     'StableDiffusion',
