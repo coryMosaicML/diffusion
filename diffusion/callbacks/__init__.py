bfc382ca0b347f71bde208675e572aff2007f1b1
Cory Stephenson <cory.stephenson@databricks.com>
2023-09-26 05:31:31 +0000

    More docstrings and code cleanup

--- diffusion/callbacks/__init__.py
+++ diffusion/callbacks/__init__.py
@@ -3,10 +3,11 @@
 
 """Custom callbacks for Diffusion."""
 
-from diffusion.callbacks.log_diffusion_images import LogDiffusionImages
+from diffusion.callbacks.log_diffusion_images import LogAutoencoderImages, LogDiffusionImages
 from diffusion.callbacks.scheduled_garbage_collector import ScheduledGarbageCollector
 
 __all__ = [
+    'LogAutoencoderImages',
     'LogDiffusionImages',
     'ScheduledGarbageCollector',
 ]
