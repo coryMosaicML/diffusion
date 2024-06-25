a0048abdabcc68ec4d7627a6e0aa2a99474d8f46
Cory Stephenson <cory.stephenson@databricks.com>
2024-06-25 18:59:36 +0000

    Prep for inference

--- diffusion/models/__init__.py
+++ diffusion/models/__init__.py
@@ -4,7 +4,8 @@
 """Diffusion models."""
 
 from diffusion.models.models import (build_autoencoder, build_diffusers_autoencoder, continuous_pixel_diffusion,
-                                     discrete_pixel_diffusion, stable_diffusion_2, stable_diffusion_xl)
+                                     discrete_pixel_diffusion, stable_diffusion_2, stable_diffusion_xl,
+                                     text_to_image_transformer)
 from diffusion.models.pixel_diffusion import PixelDiffusion
 from diffusion.models.stable_diffusion import StableDiffusion
 
@@ -17,4 +18,5 @@ __all__ = [
     'stable_diffusion_2',
     'stable_diffusion_xl',
     'StableDiffusion',
+    'text_to_image_transformer',
 ]
